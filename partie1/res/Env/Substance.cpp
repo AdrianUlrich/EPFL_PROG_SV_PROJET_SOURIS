28d049ad1c4bf07e6241540a187e7d23d4f1c17d
Adrian <37381718+AdrianUlrich@users.noreply.github.com>
2018-03-18 00:17:12 +0100

    Overhaul
    Nik la logik ce qu'ils nous demandent est debil.
    Du coup je stocke les quantites de chaque produit au lieu de stocker les fractions et tout est tellement plus simple.

--- partie1/res/Env/Substance.cpp
+++ partie1/res/Env/Substance.cpp
@@ -2,119 +2,180 @@
 #include <Utility/Constants.hpp>
 #include <Utility/Utility.hpp>
 #include <Application.hpp>
+#include <Types.hpp>
 
 #include <iostream>
 #include <algorithm>
+#include <exception>
 
 Substance::Substance()
-: cTOT(0), fVGEF(0), fGLU(0), fBMP(0)
+: cVGEF(0), cGLU(0), cBMP(0)
 {}
 
-Substance::Substance(double fVGEF, double fGLU, double fBMP)
-: 	cTOT(fVGEF + fGLU + fBMP),
-	fVGEF((cTOT)? fGLU/cTOT : 0),
-	fGLU((cTOT)? fGLU/cTOT : 0),
-	fBMP((cTOT)? fBMP/cTOT : 0),
-	SUBSTANCE_max(getAppConfig().substance_max_value)
+Substance::Substance(double cVGEF, double cGLU, double cBMP)
+:	cVGEF(cVGEF),
+	cGLU(cGLU),
+	cBMP(cBMP),
 {
 	normalise();	
 }
 
 void Substance::normalise()
 {
-	if(isNull()) 
-		cTOT = fVGEF = fGLU = fBMP = 0.0;
-	if(cTOT * fVGEF > SUBSTANCE_max)
-		fVGEF = SUBSTANCE_max /(cTOT-(cTOT * fVGEF - SUBSTANCE_max));
-	if(cTOT * fGLU > SUBSTANCE_max)
-		fGLU = SUBSTANCE_max /(cTOT-(cTOT * fGLU - SUBSTANCE_max));
-	if(cTOT * fBMP > SUBSTANCE_max)
-		fBMP = SUBSTANCE_max /(cTOT-(cTOT * fBMP - SUBSTANCE_max));
+	if (isNull()) cVGEF = cGLU = cBMP = 0.0;
+	/* potentially faster
+	if (cVGEF > cMAX)	cVGEF = cMAX;
+	if (cGLU > cMAX)	cGLU = cMAX;
+	if (cBMP > cMAX)	cBMP = cMAX;
+	if (cVGEF < 0)		cVGEF = 0;
+	if (cGLU < 0)		cGLU = 0;
+	if (cBMP < 0)		cBMP = 0;
+	*/
+	cVGEF= min(cMAX,max(0,cVGEF));
+	cGLU = min(cMAX,max(0,cGLU));
+	cBMP = min(cMAX,min(0,cBMP));
+
 }
 
-bool Substance::isNull()
+bool Substance::isNull(double precision)
 {
-	return((isEqual(fVGEF * cTOT, 0, SUBSTANCE_PRECISION))	
-		and(isEqual(fGLU * cTOT, 0, SUBSTANCE_PRECISION))
-		and(isEqual(fBMP * cTOT, 0, SUBSTANCE_PRECISION)));
+	return ((isEqual(cVGEF,0, precision))	
+		and (isEqual(cGLU, 0, precision))
+		and (isEqual(cBMP, 0, precision)));
+}
+
+double getFractVGEF() const;
+{
+	if (cVGEF==0) return 0;
+	return cVGEF/getTotalConcentration();
+}
+double getFractGlucose() const;
+{
+	if (cGLU==0) return 0;
+	return cGLU/getTotalConcentration();
+}
+double getFractInhibitor() const
+{
+	if (cBMP==0) return 0;
+	return cBMP/getTotalConcentration();
+}
+double getTotalConcentration() const
+{
+	return cVGEF + cGLU + cBMP;
 }
 
 bool Substance::operator ==(Substance const& sub) const
 {
-	return((isEqual(fVGEF*cTOT, sub[fVGEF], SUBSTANCE_PRECISION)) 
-		and(isEqual(fGLU*cTOT, sub[fGLU], SUBSTANCE_PRECISION))
-		and(isEqual(fBMP*cTOT, sub[fBMP], SUBSTANCE_PRECISION)));
+	return ((isEqual(cVGEF,sub[VGEF], 			SUBSTANCE_PRECISION)) 
+		and (isEqual(cGLU, sub[GLUCOSE], 		SUBSTANCE_PRECISION))
+		and (isEqual(cBMP, sub[BROMOPYRUVATE], 	SUBSTANCE_PRECISION)));
 }
 
 bool operator !=(Substance const& sub1, Substance const& sub2 ) const
 {
-	return not(sub1==sub2);
+	return not (sub1==sub2);
 }
 
-double Substance::operator[](const SubstanceId index) const
+double Substance::operator[](SubstanceId index) const
 {
-	switch(index)
+	switch (index)
 	{
 		case VGEF:
-			return(fVGEF * cTOT);
+			return cVGEF;
+			
 		case GLUCOSE:
-			return(fGLU * cTOT);
+			return cGLU;
+			
 		case BROMOPYRUVATE:
-			return(fBMP * cTOT);
+			return cBMP;
+			
 		default:
-			std::cerr << "Wrong index" << std::endl;
-			throw;
+			throw std::invalid_argument;
 	} 
 }
 
-ostream& operator <<(ostream& sortie, Substance const& sub)
+std::ostream& operator<<(std::ostream& sortie, Substance const& sub)
 {
-	sortie	<<"[VGEF] : "<<sub[2]
-			<<"\n[GLUCOSE] : "<<sub[0]
-			<<"\n[BROMOPYRUVATE] : "<<sub[1]
+	sortie	<<"[VGEF] : "			<<sub[VGEF]
+			<<"\n[GLUCOSE] : "		<<sub[GLUCOSE]
+			<<"\n[BROMOPYRUVATE] : "<<sub[BROMOPYRUVATE]
 			<<std::endl;
 return sortie;
 }
 
-Substance& Substance::operator +=(Substance const& sub)
+Substance& Substance::operator+=(Substance const& sub)
 {
-	double newqVGEF=min((fVGEF*cTOT+sub[VGEF]),0);
-	double newqGLU=min((fGLU*cTOT+sub[GLUCOSE]),0);
-	double newqBMP=min((fBMP*cTOT+sub[BROMOPYRUVATE]),0);
-	cTOT=(fVGEF + fGLU + fBMP);
-	fVGEF=((cTOT)? fGLU/cTOT : 0),
-	fGLU=((cTOT)? fGLU/cTOT : 0),
-	fBMP=((cTOT)? fBMP/cTOT : 0)
 	normalise();
-	return *this;
-}
-
-Substance& Substance::operator -=(Substance const& sub)
-{
-	double newqVGEF=max((fVGEF*cTOT-sub[VGEF]),SUBSTANCE_max);
-	double newqGLU=max((fGLU*cTOT-sub[GLUCOSE]),SUBSTANCE_max);
-	double newqBMP=max((fBMP*cTOT-sub[BROMOPYRUVATE]),SUBSTANCE_max);
-	cTOT=(fVGEF + fGLU + fBMP);
-	fVGEF=((cTOT)? fGLU/cTOT : 0),
-	fGLU=((cTOT)? fGLU/cTOT : 0),
-	fBMP=((cTOT)? fBMP/cTOT : 0)
-	normalise();
-	return *this;
-}
-
-
-Substance& Substance::operator *=(double scalaire)
-{
-	cTOT*=scalaire;
-	return *this;
 	
+	cVGEF+=sub[VGEF];
+	cGLU +=sub[GLUCOSE];
+	cBMP +=sub[BROMOPYRUVATE];
+	
+	normalise();
+	return *this;
 }
 
-Substance operator *(Substance const sub, double scalaire)
+Substance& Substance::operator-=(Substance const& sub)
 {
-	return sub*=scalaire;
+	normalise();
+	
+	cVGEF-=sub[VGEF];
+	cGLU -=sub[GLUCOSE];
+	cBMP -=sub[BROMOPYRUVATE];
+	
+	normalise();
+	return *this;
 }
 
-void update(SubstanceId subId, double c)
+
+Substance& Substance::operator*=(double scalaire)
 {
+	normalise();
+	
+	cVGEF*= scalaire;
+	cGLU *= scalaire;
+	cBMP *= scalaire;
+	normalise();
+	
+	return *this;	
 }
+
+Substance operator*(Substance sub, double scalaire)
+{
+	return sub *= scalaire;
+}
+
+void update(SubstanceId subId, double scalaire)
+{
+	switch (subID)
+	{
+		case VGEF:
+			cVGEF*= scalaire;
+			break;
+			
+		case GLUCOSE:
+			cGLU *= scalaire;
+			break;
+			
+		case BROMOPYRUVATE:
+			cBMP *= scalaire;
+			break;
+			
+		default:
+			throw std::invalid_argument;
+	}
+	normalise();
+}
+
+void uptakeOnGradient(double fraction, Substance& receiver, SubstanceId id)
+{
+	double grad((this->operator[](id))*(1-fraction));
+	if (grad<SUBSTANCE_PRECISION) return;
+	update(id,1-fraction);
+	reciever.update(id,(grad+reciever[id])/reciever[id]);
+}
+
+
+
+
+
--- partie1/res/Env/Substance.hpp
+++ partie1/res/Env/Substance.hpp
@@ -1,47 +1,43 @@
 #ifndef I_SUBSTANCE_HPP
 #define I_SUBSTANCE_HPP
 
-
-
+#include <Application.hpp>
+#include <Utility/Constants.hpp>
 
 class Substance 
-{
-	
+{	
 public:
-
 	Substance(double, double, double);
 	Substance();
 	Substance(const Substance& sub) = default;
+	Substance& operator=(Substance const&) = default;
 	~Substance() = default;
 	
-	bool isNull();
+	bool isNull(double precision = SUBSTANCE_PRECISION);
+	void update(SubstanceId,double);
+	void uptakeOnGradient(double,Substance&,SubstanceId);
 
-	double getFractfVGEF() const {return fVGEF;}
-	double getFractfGLU() const {return fGLU;}
-	double getFractVfBMP() const {return fBMP;}
-	double getTotalConcentration() const {return cTOT;}
+	double getFractVGEF() const;
+	double getFractGlucose() const;
+	double getFractInhibitor() const;
+	double getTotalConcentration() const;
 
-	
-	Substance& operator =(Substance const&) = default;
-	bool operator ==(Substance const&) const;
+	bool operator==(Substance const&) const;
 	double operator[](const SubstanceId index) const;
 	Substance& Substance::operator +=(Substance const&);
 	Substance& Substance::operator -=(Substance const&);
 	Substance& Substance::operator *=(double);
-	
-	
 private:
-		double SUBSTANCE_max;
+	void normalise();
 
-		void normalise(); 
+	const static double cMAX = getAppConfig().substance_max_value;
 
-		double cTOT;
-		double fVGEF;
-		double fGLU;
-		double fBMP;
+	double cVGEF;
+	double cGLU;
+	double cBMP;
 }
 
-Substance Substance::operator *(Substance);
+Substance Substance::operator *(Substance,Substance const&);
 bool operator !=(Substance const&,Substance const&) const;
 
 #endif //I_SUBSTANCE_HPP
