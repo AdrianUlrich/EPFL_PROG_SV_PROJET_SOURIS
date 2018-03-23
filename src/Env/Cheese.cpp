ed2f37a2995159195afa904b9cedaa44e4abe266
Adrian <37381718+AdrianUlrich@users.noreply.github.com>
2018-03-23 11:29:27 +0100

    nope

--- src/Env/Cheese.cpp
+++ src/Env/Cheese.cpp
@@ -1,26 +1,26 @@
-//#include "Cheese.hpp"
-//#include <Application.hpp>
-//#include <algorithm>
-//
-//Cheese::Cheese()
-//: texture(getAppConfig().cheese_texture),
-//	energy(getAppConfig().cheese_initial_energy)
-//	entitySprite
-//	(
-//		buildSprite
-//		(
-//			pos,
-//			energy,
-//			texture
-//		)
-//	),
-//	maxSide(std::max(texture.getSize().x, texture.getSize().y)
-//{}
-//
-//void Cheese::drawOn(sf::RenderTarget& target)
-//{
-//	//SimulatedEntity::drawOn(target);
-//	entitySprite.setScale(Vec2d(energy,energy)/maxSide);
-//	entitySprite.setRotation(angle/ DEG_TO_RAD);
-//	target.draw(entitySprite);
-//}
+#include "Cheese.hpp"
+#include <Application.hpp>
+#include <algorithm>
+
+Cheese::Cheese()
+: texture(getAppConfig().cheese_texture),
+	energy(getAppConfig().cheese_initial_energy)
+	entitySprite
+	(
+		buildSprite
+		(
+			pos,
+			energy,
+			texture
+		)
+	),
+	maxSide(std::max(texture.getSize().x, texture.getSize().y)
+{}
+
+void Cheese::drawOn(sf::RenderTarget& target)
+{
+	//SimulatedEntity::drawOn(target);
+	entitySprite.setScale(Vec2d(energy,energy)/maxSide);
+	entitySprite.setRotation(angle/ DEG_TO_RAD);
+	target.draw(entitySprite);
+}
