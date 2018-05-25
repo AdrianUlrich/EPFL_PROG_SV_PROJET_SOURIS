de301d7d62e3055bceee7bf70b968037763a5327
Adrian <adrian.schemel888@gmail.com>
2018-05-25 17:14:37 +0200

    jjj

--- src/Env/Substance.cpp
+++ src/Env/Substance.cpp
@@ -8,17 +8,20 @@
 #include <algorithm>
 #include <exception>
 
+const double Substance::cMAX = getAppConfig().substance_max_value;
+
 Substance::Substance()
-: cVGEF(0), cGLU(0), cBMP(0)
+  : cVGEF(0)
+  , cGLU(0)
+  , cBMP(0)
 {}
 
 Substance::Substance(double cVGEF, double cGLU, double cBMP)
-:	cMAX(getAppConfig().substance_max_value),
-	cVGEF(cVGEF),
-	cGLU(cGLU),
-	cBMP(cBMP)
+  :	cVGEF(cVGEF)
+  ,	cGLU(cGLU)
+  ,	cBMP(cBMP)
 {
-	normalise();	
+	normalise();
 }
 
 void Substance::normalise()
@@ -30,7 +33,7 @@ void Substance::normalise()
 	if (cVGEF < SUBSTANCE_PRECISION)	cVGEF = 0;
 	if (cGLU < SUBSTANCE_PRECISION)		cGLU = 0;
 	if (cBMP < SUBSTANCE_PRECISION)		cBMP = 0;
-	
+
 	/*
 	cVGEF= min(cMAX,max(0,cVGEF));
 	cGLU = min(cMAX,max(0,cGLU));
@@ -40,7 +43,7 @@ void Substance::normalise()
 
 bool Substance::isNull(double precision)
 {
-	return ((isEqual(cVGEF,0, precision))	
+	return ((isEqual(cVGEF,0, precision))
 		and (isEqual(cGLU, 0, precision))
 		and (isEqual(cBMP, 0, precision)));
 }
@@ -67,7 +70,7 @@ double Substance::getTotalConcentration() const
 
 bool Substance::operator ==(Substance const& sub) const
 {
-	return ((isEqual(cVGEF,sub[VGEF], 			SUBSTANCE_PRECISION)) 
+	return ((isEqual(cVGEF,sub[VGEF], 			SUBSTANCE_PRECISION))
 		and (isEqual(cGLU, sub[GLUCOSE], 		SUBSTANCE_PRECISION))
 		and (isEqual(cBMP, sub[BROMOPYRUVATE], 	SUBSTANCE_PRECISION)));
 }
@@ -77,22 +80,22 @@ bool operator !=(Substance const& sub1, Substance const& sub2 )
 	return not (sub1==sub2);
 }
 
-double Substance::operator[](SubstanceId index) const
+double const& Substance::operator[](SubstanceId index) const
 {
 	switch (index)
 	{
 		case VGEF:
 			return cVGEF;
-			
+
 		case GLUCOSE:
 			return cGLU;
-			
+
 		case BROMOPYRUVATE:
 			return cBMP;
-			
+
 		default:
 			throw std::invalid_argument("Valid arguments are [0,1,2]");
-	} 
+	}
 }
 
 std::ostream& operator<<(std::ostream& sortie, Substance const& sub)
@@ -107,11 +110,11 @@ return sortie;
 Substance& Substance::operator+=(Substance const& sub)
 {
 	normalise();
-	
+
 	cVGEF+=sub[VGEF];
 	cGLU +=sub[GLUCOSE];
 	cBMP +=sub[BROMOPYRUVATE];
-	
+
 	normalise();
 	return *this;
 }
@@ -119,11 +122,11 @@ Substance& Substance::operator+=(Substance const& sub)
 Substance& Substance::operator-=(Substance const& sub)
 {
 	normalise();
-	
+
 	cVGEF-=sub[VGEF];
 	cGLU -=sub[GLUCOSE];
 	cBMP -=sub[BROMOPYRUVATE];
-	
+
 	normalise();
 	return *this;
 }
@@ -132,16 +135,16 @@ Substance& Substance::operator-=(Substance const& sub)
 Substance& Substance::operator*=(double scalaire)
 {
 	normalise();
-	
+
 	cVGEF*= scalaire;
 	cGLU *= scalaire;
 	cBMP *= scalaire;
 	normalise();
-	
-	return *this;	
+
+	return *this;
 }
 
-Substance operator*(Substance sub, double scalaire)
+Substance& operator*(Substance sub, double scalaire)
 {
 	return sub *= scalaire;
 }
@@ -155,15 +158,15 @@ void Substance::update(SubstanceId subId, double scalaire)
 		case 2: // VGEF
 			cVGEF*= scalaire;
 			break;
-			
+
 		case 0: // GLUCOSE
 			cGLU *= scalaire;
 			break;
-			
+
 		case 1: // BROMOPYRUVATE
 			cBMP *= scalaire;
 			break;
-			
+
 		default:
 			throw std::invalid_argument("Valid arguments are [0,1,2]");
 	}
@@ -182,7 +185,7 @@ void Substance::uptakeOnGradient(double c, Substance& receiver, SubstanceId id)
 			receiver.cVGEF+=oho;
 		}
 		break;
-			
+
 		case SubstanceId::GLUCOSE :
 		{
 			double oho(c*cGLU);
@@ -191,7 +194,7 @@ void Substance::uptakeOnGradient(double c, Substance& receiver, SubstanceId id)
 			receiver.cGLU+=oho;
 		}
 		break;
-		
+
 		case SubstanceId::BROMOPYRUVATE :
 		{
 			double oho(c*cBMP);
@@ -200,14 +203,14 @@ void Substance::uptakeOnGradient(double c, Substance& receiver, SubstanceId id)
 			receiver.cBMP+=oho;
 		}
 		break;
-		
+
 		default:
 			throw std::invalid_argument("Valid arguments are [0,1,2]");
 	}
 	normalise();
-	
-	
-	
+
+
+
 	/*
 	double grad((this->operator[](id))*c);
 	if (grad<SUBSTANCE_PRECISION) return;
@@ -217,15 +220,15 @@ void Substance::uptakeOnGradient(double c, Substance& receiver, SubstanceId id)
 		case 2: // VGEF
 			cVGEF-=grad;
 			break;
-			
+
 		case 0: // GLUCOSE
 			cGLU -= grad;
 			break;
-			
+
 		case 1: // BROMOPYRUVATE
 			cBMP -= grad;
 			break;
-			
+
 		default:
 			throw std::invalid_argument("Valid arguments are [0,1,2]");
 	}
--- src/Env/Substance.hpp
+++ src/Env/Substance.hpp
@@ -4,43 +4,149 @@
 #include <Types.hpp>
 #include <Utility/Constants.hpp>
 
-class Substance 
-{	
-public:
-	Substance(double, double, double);
-	Substance();
-	Substance(const Substance& sub) = default;
-	Substance& operator=(Substance const&) = default;
-	~Substance() = default;
-	
-	bool isNull(double precision = SUBSTANCE_PRECISION);
-	void update(SubstanceId,double);
-	void uptakeOnGradient(double,Substance&,SubstanceId);
 
+/** \brief Conteneur pour substances et methodes
+ * utillitaires pour leur manipulation
+ */
+class Substance
+{
+public:
+
+  /** \brief Constructeur utilisant des concentrations
+   * initiales
+   *
+   * \param cVGEF quantité de VGEF
+   * \param cGLU quantité de glucose
+   * \param cBMP quantitéé de bromopyruvate
+   */
+  Substance(double cVGEF, double cGLU, double cBMP);
+
+  /** \brief Constructeur par défaut initialisant
+   * les concentrations à zéro
+   */
+	Substance();
+
+  /** \brief Constructeur de copie par défaut
+   */
+	Substance(const Substance& sub) = default;
+
+  /** \brief Opérateur d'assignement par défaut
+   *
+   * \param Substance à assigner
+   * \return L'instance assignée
+   */
+	Substance& operator=(Substance const&) = default;
+
+  /** \brief Destructeur par défaut
+   */
+	~Substance() = default;
+
+  /**
+   * \param precision (par défaut SUBSTANCE_PRECISION)
+   * \return vrai si les quantités sont nulles
+   */
+	bool isNull(double precision = SUBSTANCE_PRECISION);
+
+  /** \brief Multiplie la valeur d'une concentration
+   * spécifiée
+   *
+   * \param SubstanceId La substance à modifier
+   * \param scalaire Le paramètre de modification
+   * (Si 0, annule toutes les quantités)
+   * \return void
+   */
+	void update(SubstanceId,double scalaire);
+
+  /** \brief Prend une fraction d'une concentration de
+   * l'instance courante et la donne à une autre
+   * instance
+   *
+   * Si la quantité à transférer est inférieure à
+   * SUBSTANCE_PRECISION, on ne transfère pas
+   *
+   * \param double fraction à transférer
+   * \param Substance& receveur
+   * \param SubstanceId Substance à transférer
+   *
+   * \return void
+   */
+	void uptakeOnGradient(double c,Substance&,SubstanceId);
+
+  /**
+   * \return Fraction de VGEF
+   */
 	double getFractVGEF() const;
+
+  /**
+   * \return Fraction de glucose
+   */
 	double getFractGlucose() const;
+
+  /**
+   * \return Fraction de bromopyruvate
+   */
 	double getFractInhibitor() const;
+
+  /**
+   * \return Concentration totale
+   */
 	double getTotalConcentration() const;
 
+  /** \brief Comparaison entre instances Substance
+   * \return vrai si identiques à SUBSTANCE_PRECISION près
+   */
 	bool operator==(Substance const&) const;
-	double operator[](SubstanceId index) const;
+
+  /** \brief Accès en lecture seule des quantités
+   * \param SubstanceId Lla quantité à lire
+   */
+	double const& operator[](SubstanceId) const;
+
+  /** \brief Addition entre instances Substance
+   */
 	Substance& operator +=(Substance const&);
+
+  /** \brief Soustraction entre instances Substance
+   */
 	Substance& operator -=(Substance const&);
+
+  /** \brief Multiplication d'une substance par un scalaire
+   */
 	Substance& operator *=(double);
-	
+
 
 private:
-	void normalise();
-	
-	double cMAX;
 
-	double cVGEF;
-	double cGLU;
-	double cBMP;
+  /** \brief Méthode privée responsable de restreindre
+   * les valeurs des substances entre SUBSTANCE_PRECISION
+   * et substance_max_value
+   */
+	void normalise();
+
+private:
+
+	static const double cMAX; /**< substance_max_value */
+
+	double cVGEF; /**< quantité de VGEF */
+	double cGLU; /**< quantité de glucose */
+	double cBMP; /**< quantité de bromopyruvate */
 };
 
-Substance operator*(Substance,double);
+/** \brief multiplication par un scalaire
+ *
+ * \param Substance à multiplier
+ * \param scalaire
+ */
+Substance& operator*(Substance,double);
+
+/** \brief Comparaison entre instances de substance
+ * \return l'inverse de l'opérateur==
+ */
 bool operator!=(Substance const&,Substance const&);
+
+/** \brief surcharge de l'opérateur de flux pour affichage
+ * dans la console
+ */
 std::ostream& operator<<(std::ostream&, Substance const&);
 
 #endif //I_SUBSTANCE_HPP
